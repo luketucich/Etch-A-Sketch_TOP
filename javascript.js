da3f6a992059799f4900c03f357d8d06843d4ff5
luketucich <ltucich@gmail.com>
2024-05-07 16:26:49 -0400

    The grid container is now set to a fixed size and the grid squares dynamically adjust to fit that contaienr equally.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,14 @@
 // Creates the grid
 const gridContainer = document.getElementById("gridContainer");
-var gridSize = 16 * 16;
+var gridSize = 64;
 var gridSizeSqrt = Math.sqrt(gridSize);
 
 for (let i = 0; i < gridSize; i++) {
     let gridSquare = document.createElement("div");
     gridSquare.classList.add("gridSquare");
+    // Adjusts the squares in the grid to fit the grid container
     gridSquare.setAttribute("style", `flex: 0 0 calc(100%/${gridSizeSqrt}); `);
     gridContainer.appendChild(gridSquare);
 }
 // 
 
-// Adjusts grid container width based off grid size
-// var gridContainerWidth = gridSizeSqrt * 25;
-// gridContainer.setAttribute("style", `width: ${gridContainerWidth};`);
